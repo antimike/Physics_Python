2a0201fc053e4609c807743aadee3273b6dbea46
Michael Haynes <Antimike.Haynes@colostate.edu>
2021-03-02 03:39:38 -0700

    Added extensive notes and ideas; pasted some more decorator ideas from latex_serializer

--- Utilities/functional_base_classes.py
+++ Utilities/functional_base_classes.py
@@ -65,8 +65,8 @@ class Set:
     def contains(self, val):
         return self._contains.describes(val)
     all_sets = Set(Boolean(lambda x: isinstance(x, Set)))
-    @staticmethod
-    def union(first, second):
+    # @staticmethod
+    # def union(first, second):
 
 class Function:
     def __init__(self, f, domain, codomain):
@@ -91,7 +91,77 @@ def method_options_filter(instance_kwargs_fn):
         instance_kwargs_fn
     )
 
+## Decorator cemetery
+
 get_opts = method_decorator(
     lambda instance, *args, **kwargs: args,
     lambda instance, **kwargs: {**instance._opts, **kwargs}
 )
+
+def apply_defaults(fn):
+    def wrapper(obj, *args, **kwargs):
+        return fn(obj, *args, **{**obj._opts, **kwargs})
+    return wrapper
+
+@bind_args(compose_reducers)
+def bind_kwargs(*reducers):
+    # reducer = lambda instance, reducers, kwargs: 
+    def decorator(fn):
+        def wrapper(instance, *args, **kwargs):
+            return fn(instance, *args, reducer(instance, kwargs))
+        return wrapper
+    return decorator
+
+# A reducer f: (S, S) -> S can be "lifted" to a function (S, S) -> (S, S) in three obvious ways.
+# Two ways are "conservative:" (S_1, S_2) |-> (S_1, r(S_1, S_2)) and the adjoint, each of which fixes one of the subdomains.
+# The difference between these two can be thought of as "immediate state update" (right arg fixed, left mutable) and "accumulated state update deferred" (left arg fixed representing the state to be updated; right arg mutable)
+# The other is "forgetful:" (S_1, S_2) |-> (S_2, r(S_1, S_2))
+# If the state space is itself structured in some algebraic way, and if reducers are assumed to respect this structure, then under certain conditions these three possibilities might constitute some sort of "basis" for the set of *all* possible ways of lifting a reducer to a function (S, S) -> S.
+
+# This can all be made sense of if we think of reducers as contravariant or covariant, like tensors but without the assumed linear structure.
+# Essentially, the pair (S_1, S_1) splits into "state vectors" (tangent to some manifold or generalization thereof, perhaps) and "update forms" (cotangents).
+# The type of the return value of r determines its classification.
+
+# Of course, the specification of any pair of covariant and contravariant reducers would be enough to construct the entire future "update trajectory" given an initial seed state and a single update event.
+# Thus, this model might be too idealistic.
+# Instead, we should think about "half-covariant" reducers---reducers which return future updates with some degree of uncertainty or imprecision---being paired with fully contravariant reducers used to actually update the internal state as events unfold.
+# Or perhaps we should think of the state itself acquiring "fuzziness" or uncertainty over time, in which case the contravariant reducers are also in reality only "partly" or "imperfectly" covariant.
+
+# Analogy to physics: Anomalous circumstances in which an "update" (impulse) fails to result in the usual displacement (application of reducer) can be interpreted as the result of:
+# - noninertial frame
+# - the presence of some external force
+# - inertia?
+
+# "Discrete quantization" of a reduction framework...
+# ...would be to imagine a superposition of class instances as being the "state" to be updated
+# ...or could be a superposition of states ("fuzzy reducers") for a fixed class instance, depending on the interpretation of the class.
+
+# How best to represent reducers in class form, in Python?
+"""
+'Reducer' should expose:
+* reduce
+* left-semilift
+* right-semilift
+* skew-semilift
+* contra-/co-variance
+"""
+
+def right_compose_reducers(reducers):
+    def reducer(first, second):
+        for r in reducers:
+
+
+def bind_args(reducer):
+    def decorator(fn):
+        def wrapper(instance, *args, **kwargs):
+            return fn(instance, *reducer(instance, args), kwargs)
+        return wrapper
+    return decorator
+
+def reduce_data_opts(table, opts):
+    return {**table._opts, **opts}
+
+def reduce_col_title_opts(table, opts):
+    data_kwargs = reduce_data_opts(table, opts)
+    col_title_opts = {**table._opts['col_title_opts'], **opts, **opts['col_title_opts']}
+    return {**reduce_data_opts(table, opts), 'col_title_opts': col_title_opts}
