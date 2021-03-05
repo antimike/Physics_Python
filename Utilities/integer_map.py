6c0f2e8473995daf5d67b46ad7157a8c0258d0c5
Michael Haynes <Antimike.Haynes@colostate.edu>
2021-03-05 00:07:00 -0700

    Added explanatory comments to 'integer_map.py'

--- Utilities/integer_map.py
+++ Utilities/integer_map.py
@@ -1,11 +1,34 @@
+"""
+How to invert a boolean sort (subset selection) of a list:
+first = [x for test(x) and x in l]
+second = [x for not test(x) and x in l]
+reconstructed = first + second
+original = (reverse + split(test) + reverse)(reconstructed)
+"""
+
+"""
+Explanation: "Boolean sort" specification of subsets / integer maps
+Imagine a Boolean function guaranteed to return a result, say P (predicate).
+Given a collection `list`, we can form `list1 = filter(P, list)` and `list2 = filter(not P, list)`.
+We can use this construction to do several things:
+
+1. Most obviously, we can think of `list1` as a subset of `list` parameterized by P.
+So any function that calls for a map `S::Set -> S'::Set : S.contains(S')` can be passed P instead.
+
+2. Alternatively, we can concatenate the two lists: `scrambled = list1 + list2`.
+The result is formally a permutation `sigma` of the set `range(1, len(list))`.
+In particular, we can ask what the inverse of `sigma` is in terms of P.
+More interestingly:  Can this or similar constructions be used to parameterize *all* permutations?
+Is there a reasonable subset of S_N that admits a particularly "nice" description in terms of such operators?
+"""
+
 def scramble(predicate, args):
     selected = [x for i, x in enumerate(args) if predicate(i)]
     unselected = [x for i, x in enumerate(args) if not predicate(i)]
     return [*selected, *unselected]
 
 
-def unscramble(predicate, beep_boop, debug=False):
-    args = beep_boop
+def unscramble(predicate, args, debug=False):
     args.reverse()
     if debug:
         print('-----' + str(args))
