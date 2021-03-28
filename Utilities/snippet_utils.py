06489ed6b27f638560fd83e6a161f56f8eeb08d7
Michael Haynes <Antimike.Haynes@colostate.edu>
2021-03-27 23:42:09 -0600

    Added 'mathmode' array and array of braces and matching delimiters to 'snippet_utils.py'

--- Utilities/snippet_utils.py
+++ Utilities/snippet_utils.py
@@ -1,4 +1,22 @@
 import sys
+import vim
+
+texMathZones = ['texMathZone' + x for x in ['A', 'AS', 'B', 'BS', 'C', 'CS',
+'D', 'DS', 'E', 'ES', 'F', 'FS', 'G', 'GS', 'H', 'HS', 'I', 'IS', 'J', 'JS',
+'K', 'KS', 'L', 'LS', 'DS', 'V', 'W', 'X', 'Y', 'Z', 'AmsA', 'AmsB', 'AmsC',
+'AmsD', 'AmsE', 'AmsF', 'AmsG', 'AmsAS', 'AmsBS', 'AmsCS', 'AmsDS', 'AmsES',
+'AmsFS', 'AmsGS' ]] + ["VimwikiMath", "VimwikiEqIn"]
+texIgnoreMathZones = ['texMathText']
+texMathZoneIds = vim.eval('map('+str(texMathZones)+", 'hlID(v:val)')")
+texIgnoreMathZoneIds = vim.eval('map('+str(texIgnoreMathZones)+", 'hlID(v:val)')")
+ignore = texIgnoreMathZoneIds[0]
+def is_math_mode():
+    synstackids = vim.eval("synstack(line('.'), col('.') - (col('.')>=2 ? 1 : 0))")
+    try:
+        first = next(i for i in reversed(synstackids) if i in texIgnoreMathZoneIds or i in texMathZoneIds)
+        return first != ignore
+    except StopIteration:
+        return False
 
 def get_digits(s):
     return int(''.join(filter(str.isdigit, s)))
@@ -11,3 +29,6 @@ def substitute_text(text, snip):
     snip.buffer = snip.buffer[:first] \
         + text[:-1] \
         + snip.buffer[last:]
+
+_braces_lr = {r"(": r")", r"[": r"]", r"{": r"}", r"\{": r"\}"}
+braces = {**_braces_lr, **{v: k for k, v in _braces_lr.items()}}
